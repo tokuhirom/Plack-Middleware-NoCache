ce4187a4cf56158af06757f6d9a665881bef746b
tokuhirom <tokuhirom@gmail.com>
2010-09-15 23:52:59 +0900

    initial import

---
+++ lib/Plack/Middleware/NoCache.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::NoCache;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::Middleware::NoCache -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::NoCache;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::NoCache is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
